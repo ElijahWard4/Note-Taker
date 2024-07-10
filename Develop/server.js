cf31fdba1670b99af0972e8d93af71057ef709ea
EliWard4 <elijah.ward014@gmail.com>
2024-07-10 11:04:48 -0500

    Middleware for parsing JSON and urlencoded form data and serving static files

--- Develop/server.js
+++ Develop/server.js
@@ -6,3 +6,8 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 const dbPath = path.join(__dirname, 'db', 'db.json');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
